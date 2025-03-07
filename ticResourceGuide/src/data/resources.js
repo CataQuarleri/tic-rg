65475252f17122c3eee1826292a5ddce9c77388b
CataQu <cataquarleri@gmail.com>
2025-03-06 17:42:12 -0700

    add: resources

--- ticResourceGuide/src/data/resources.js
+++ ticResourceGuide/src/data/resources.js
@@ -1,79 +1,296 @@
 export const resources = [
-    {
-      population: "youth",
-      time: 90,
-      title: "Implementing a Trauma-Informed Approach for Youth Across Sectors",
-      type: "video",
-      url: "https://www.youtube.com/embed/35FwmVUYLMw"
+  {
+    population: "youth",
+    time: 90,
+    title: "Implementing a Trauma-Informed Approach for Youth Across Sectors",
+    type: "video",
+    url: "https://www.youtube.com/embed/35FwmVUYLMw"
+  },
+  {
+      population: "general",
+      time: 5,
+      title: "What is Trauma Informed Care?",
+      type: "article",
+      url: "https://traumainformedoregon.org/wp-content/uploads/2016/01/What-is-Trauma-Informed-Care.pdf"
     },
     {
-        url: "https://traumainformedoregon.org/wp-content/uploads/2016/01/What-is-Trauma-Informed-Care.pdf",
-        time: 5,
-        title: "What is Trauma Informed Care?",
-        type: "article",
-        population: "General"
-      },
-      {
-        url: "https://nnedv.org/spotlight_on/understanding-importance-trauma-informed-care/",
-        time: 4,
-        title: "Understanding the Importance of Trauma-Informed Care",
-        type: "article",
-        population: "General"
-      },
-      {
-        url: "https://www.bidmc.org/-/media/files/beth-israel-org/centers-and-departments/social-work/center-for-violence-prevention-and-recovery/bidmc-cvpr-trauma-informed-care-tips-sheet-062921.pdf",
-        time: 7,
-        title: "What is Trauma Informed Care?",
-        type: "article",
-        population: "Healthcare"
-      },
-      {
-        url: "https://www.nctsn.org/trauma-informed-care/creating-trauma-informed-systems",
-        time: 2,
-        title: "Creating Trauma-Informed Systems",
-        type: "article",
-        population: "Youth"
-      },
-      {
-        url: "https://www.nctsn.org/trauma-informed-care/culture-and-trauma",
-        time: 2,
-        title: "Culture and Trauma",
-        type: "article",
-        population: "General"
-      },
-      {
-        url: "https://www.nctsn.org/trauma-informed-care/families-and-trauma",
-        time: 2,
-        title: "Families and Trauma",
-        type: "article",
-        population: "Families"
-      },
-      {
-        url: "https://www.nctsn.org/trauma-informed-care/nctsn-trauma-informed-organizational-assessment/what-trauma-informed-care-and-why-it",
-        time: 2,
-        title: "What is Trauma-Informed Care and Why It Matters?",
-        type: "article",
-        population: "General"
-      },
-      {
-        url: "https://www.nctsn.org/trauma-informed-care/creating-trauma-informed-systems/healthcare",
-        time: 2,
-        title: "Healthcare",
-        type: "article",
-        population: "Healthcare"
-      },
-      {
-        url: "https://www.nctsn.org/trauma-informed-care/trauma-informed-systems/justice/essential-elements",
-        time: 2,
-        title: "Juvenile Justice-Involved Youth",
-        type: "article",
-        population: "Youth"
-      },
-      {
-        url: "https://www.nctsn.org/trauma-informed-care/trauma-informed-systems/trauma-informed-pediatric-psychiatry",
-        time: 3,
-        title: "Trauma-Informed Pediatric Psychiatry",
-        type: "article",
-        population: "Youth"
-      }
-  ];
+      population: "general",
+      time: 4,
+      title: "Understanding the Importance of Trauma-Informed Care",
+      type: "article",
+      url: "https://nnedv.org/spotlight_on/understanding-importance-trauma-informed-care/"
+    },
+    {
+      population: "healthcare",
+      time: 7,
+      title: "What is Trauma Informed Care",
+      type: "article",
+      url: "https://www.bidmc.org/-/media/files/beth-israel-org/centers-and-departments/social-work/center-for-violence-prevention-and-recovery/bidmc-cvpr-trauma-informed-care-tips-sheet-062921.pdf"
+    },
+    {
+      population: "youth",
+      time: 4,
+      title: "Creating Trauma-Informed Systems",
+      type: "article",
+      url: "https://www.nctsn.org/trauma-informed-care/creating-trauma-informed-systems"
+    },
+    {
+      population: "general",
+      time: 2,
+      title: "Culture and Trauma",
+      type: "article",
+      url: "https://www.nctsn.org/trauma-informed-care/culture-and-trauma"
+    },
+    {
+      population: "families",
+      time: 2,
+      title: "Families and Trauma",
+      type: "article",
+      url: "https://www.nctsn.org/trauma-informed-care/families-and-trauma"
+    },
+    {
+      population: "general",
+      time: 2,
+      title: "What is Trauma-Informed Care and Why It Matters?",
+      type: "article",
+      url: "https://www.nctsn.org/trauma-informed-care/nctsn-trauma-informed-organizational-assessment/what-trauma-informed-care-and-why-it"
+    },
+    {
+      population: "youth",
+      time: 2,
+      title: "Healthcare",
+      type: "article",
+      url: "https://www.nctsn.org/trauma-informed-care/creating-trauma-informed-systems/healthcare"
+    },
+    {
+      population: "youth",
+      time: 2,
+      title: "Juvenile Justice-Involved Youth",
+      type: "article",
+      url: "https://www.nctsn.org/trauma-informed-care/trauma-informed-systems/justice/essential-elements"
+    },
+    {
+      population: "youth",
+      time: 3,
+      title: "Trauma-Informed Pediatric Psychiatry",
+      type: "article",
+      url: "https://www.nctsn.org/trauma-informed-care/trauma-informed-systems/trauma-informed-pediatric-psychiatry"
+    },
+    {
+      population: "general",
+      time: 3,
+      title: "PTSD: National Center for PTSD ",
+      type: "article",
+      url: "https://www.ptsd.va.gov/professional/treat/care/index.asp"
+    },
+    {
+      population: "general",
+      time: 15,
+      title: "Trauma-Informed Therapy ",
+      type: "research-paper",
+      url: "https://www.ncbi.nlm.nih.gov/books/NBK604200/"
+    },
+    {
+      population: "general",
+      time: 5,
+      title: "Missouri Department of Mental Health - Trauma Informed Care",
+      type: "article",
+      url: "https://dmh.mo.gov/trauma-informed-care"
+    },
+    {
+      population: "general",
+      time: 26,
+      title: "Introduction to Trauma Introduction to Trauma – Dr. Patsy Carter",
+      type: "video",
+      url: "https://mimhtraining.com/introduction-to-trauma/#page-content"
+    },
+    {
+      population: "general",
+      time: 4,
+      title: "Missouri Institute of Mental Health  Fact Sheet",
+      type: "article",
+      url: "https://dmh.mo.gov/media/pdf/trauma-fact-sheet"
+    },
+    {
+      population: "general",
+      time: 3,
+      title: "Jack, Joseph and Morton Mandel School of Applied Social Sciences Trauma Informed Care",
+      type: "article",
+      url: "https://case.edu/socialwork/centerforebp/practices/trauma-informed-care"
+    },
+    {
+      population: "general",
+      time: 5,
+      title: "What is Trauma Informed Care? Background Information About Trauma Informed Care ",
+      type: "article",
+      url: "https://traumainformedoregon.org/resources/new-to-trauma-informed-care/what-is-trauma-informed-care/"
+    },
+    {
+      population: "general",
+      time: 6,
+      title: "Key Terms Related to Realizing the Widespread Impact of Trauma",
+      type: "article",
+      url: "https://traumainformedoregon.org/wp-content/uploads/2020/06/Key-Terms-Related-to-Realizing-the-Widespread-Impact-of-Trauma.pdf"
+    },
+    {
+      population: "general",
+      time: 2,
+      title: "Interagency Task Force on Trauma-Informed Care ",
+      type: "article",
+      url: "https://www.samhsa.gov/mental-health/trauma-violence/trauma-informed-care"
+    },
+    {
+      population: "general",
+      time: 78,
+      title: "Practical Guide for Implementing a Trauma-Informed Approach",
+      type: "research-paper",
+      url: "https://library.samhsa.gov/sites/default/files/pep23-06-05-005.pdf"
+    },
+    {
+      population: "youth",
+      time: 65,
+      title: "From the Academy of Pediatrics - Trauma-Informed Care",
+      type: "research-paper",
+      url: "https://publications.aap.org/pediatrics/article/148/2/e2021052580/179745/Trauma-Informed-Care?autologincheck=redirected"
+    },
+    {
+      population: "youth",
+      time: 42,
+      title: "The Importance of a Trauma-Informed Child Welfare System",
+      type: "research-paper",
+      url: "https://cwig-prod-prod-drupal-s3fs-us-east-1.s3.amazonaws.com/public/documents/trauma_informed.pdf?VersionId=UC.ccnAsGhYNv.UhH4lguLaF9Jc5gMWz"
+    },
+    {
+      population: "youth",
+      time: 60,
+      title: "A Trauma-Informed Guide for Working with Youth Involved in Multiple Systems",
+      type: "research-paper",
+      url: "https://www.nctsn.org/sites/default/files/resources/resource-guide/a-trauma-informed-guide-for-working-with-youth-involved-in-multiple-systems.pdf"
+    },
+    {
+      population: "general",
+      time: 11,
+      title: "Southern New Hampshire - What is Trauma-Informed Care?",
+      type: "article",
+      url: "https://www.snhu.edu/about-us/newsroom/health/what-is-trauma-informed-care"
+    },
+    {
+      population: "implementation",
+      time: 4,
+      title: "Implementing Trauma-Informed Practices throughout the San Francisco Department of Public Health ",
+      type: "article",
+      url: "https://www.traumainformedcare.chcs.org/wp-content/uploads/2018/11/SFDPH-Profile.pdf"
+    },
+    {
+      population: "general",
+      time: 6,
+      title: "Addressing Trauma in Health Care, Schools, and the Community: Greater Newark Healthcare Coalition",
+      type: "article",
+      url: "https://www.traumainformedcare.chcs.org/wp-content/uploads/2018/11/GNHCC-Profile.pdf"
+    },
+    {
+      population: "general",
+      time: 3,
+      title: "What is Trauma-Informed Care?",
+      type: "article",
+      url: "https://www.traumainformedcare.chcs.org/what-is-trauma-informed-care/"
+    },
+    {
+      population: "general",
+      time: 4,
+      title: "",
+      type: "video",
+      url: "https://www.youtube.com/watch?v=fWken5DsJcw&t=2s"
+    },
+    {
+      population: "general",
+      time: 2,
+      title: "Advancing Trauma Informed Care",
+      type: "article",
+      url: "https://www.chcs.org/project/advancing-trauma-informed-care/"
+    },
+    {
+      population: "youth",
+      time: 7,
+      title: "Trauma Informed Care or Trauma Informed Services",
+      type: "article",
+      url: "http://www.traumainformedcareproject.org/resources/SEC-NL-40019.ChildWelfareNL-Reprint.pdf"
+    },
+    {
+      population: "general",
+      time: 40,
+      title: "Trauma Informed Care: An Ecological Response",
+      type: "article",
+      url: "http://www.traumainformedcareproject.org/resources/Trauma-Informed-Care-An-Ecological-Response-Guarino-2015.pdf"
+    },
+    {
+      population: "youth",
+      time: 22,
+      title: "Medical Traumatic Stress",
+      type: "article",
+      url: "http://www.traumainformedcareproject.org/resources/pediatric_toolkit_for_parents1.pdf"
+    },
+    {
+      population: "general",
+      time: 17,
+      title: "Laying the Groundwork for Trauma-Informed Care",
+      type: "article",
+      url: "http://www.traumainformedcareproject.org/resources/Laying-the-Groundwork-for-TIC_012418.pdf"
+    },
+    {
+      population: "general",
+      time: 7,
+      title: "Why Schools Need to Be Trauma Informed",
+      type: "article",
+      url: "http://www.traumainformedcareproject.org/resources/WhySchoolsNeedToBeTraumaInformed(2).pdf"
+    },
+    {
+      population: "general",
+      time: 18,
+      title: "Creating trauma-informed correctional care: a balance of goals and environment",
+      type: "research-paper",
+      url: "http://www.traumainformedcareproject.org/resources/2012%20miller%20naj%20trm%20corrs%20ejp.pdf"
+    },
+    {
+      population: "general",
+      time: 13,
+      title: "Creating a Trauma-Informed Criminal Justice System for Women: Why and How",
+      type: "research-paper",
+      url: "http://www.traumainformedcareproject.org/resources/TIC%20criminal%20justice%20for%20women%20(2).pdf"
+    },
+    {
+      population: "general",
+      time: 6,
+      title: "The 6 Core Principles of Trauma-Informed Care and Their Role in Trauma Therapy",
+      type: "article",
+      url: "https://reachbh.org/6-core-principles-of-trauma-informed-care/"
+    },
+    {
+      population: "general",
+      time: 6,
+      title: "Awareness of Trauma-Informed Care",
+      type: "article",
+      url: "https://www.socialworktoday.com/archive/exc_012014.shtml"
+    },
+    {
+      population: "general",
+      time: 18,
+      title: "Trauma and Trauma-Informed Care: An Introductory Training Provided by DFPS",
+      type: "article",
+      url: "https://www.dfps.texas.gov/Training/Trauma_Informed_Care/documents/Trauma_Informed_Care_Training.pdf"
+    },
+    {
+      population: "general",
+      time: 3,
+      title: "Trauma-Informed Care  - What is it?",
+      type: "article",
+      url: "https://www.traumapolicy.org/topics/trauma-informed-care"
+    },
+    {
+      population: "general",
+      time: 4,
+      title: "3 Pillars & Principles of a Trauma-Informed Approach    ",
+      type: "article",
+      url: "https://ctrinstitute.com/blog/trauma-informed/"
+    }
+];
